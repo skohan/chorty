ab2416c7496121c308c81502260159990aeefd60
Sohan <sohan@email.com>
2021-01-20 12:25:21 +0530

    Updated login ui

--- shorty/models.py
+++ shorty/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     user = models.ForeignKey(auth_models.User, on_delete= models.CASCADE) 
     is_premium = models.BooleanField(default=False)
     no_of_urls = models.IntegerField(default=0)
+    email = models.EmailField(primary_key=True)
 
     def __str__(self):
         return f"{self.user} | {self.is_premium}"
